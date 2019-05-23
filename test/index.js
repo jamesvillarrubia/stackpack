c4502fdedc592d70461e8c1f6fe2929414e9f08e
James Villarrubia <james.villarrubia@gmail.com>
2019-05-23 18:39:46 -0400

    updates build I think

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 import { assert } from 'chai';
 // import defaultAwesomeFunction, { awesomeFunction } from '../src';
+import build from '../lib/cmds/build';
 
 describe('Awesome test.', () => {
   it('should test default awesome function', () => {
+    assert(typeof build === 'function');
     assert(true);
   });
 
